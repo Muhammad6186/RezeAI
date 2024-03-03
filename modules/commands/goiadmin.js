6553bb92232e6b1dadef1417febba22567403daf
Marjhxn <90974141+Muhammad6186@users.noreply.github.com>
2024-03-03 15:02:01 +0000

    update

--- modules/commands/goiadmin.js
+++ modules/commands/goiadmin.js
@@ -6,17 +6,27 @@ module.exports.config = {
   description: "Bot will rep ng tag admin or rep ng tagbot ",
   commandCategory: "Other",
   usages: "",
-  cooldowns: 1
+  cooldowns: 1,
 };
-module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100016878310988") {
-    var aid = ["100016878310988"];
+module.exports.handleEvent = function ({ api, event }) {
+  if (event.senderID !== "100043265301021") {
+    var aid = ["100043265301021"];
     for (const id of aid) {
-    if ( Object.keys(event.mentions) == id) {
-      var msg = ["Tinatag pa anong satin ha", "aba aba tinatag nga bat ba ha???!!!", "sinasabi ko sayo iba ako magalit bat moba tinatag admin ko, ay be gusto mo ba owner ko?","hala sorry idolo wala pa si sam ih"," hala hinahanap si sam miss mo ayieee."];
-      return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+      if (Object.keys(event.mentions) == id) {
+        var msg = [
+          "Tinatag pa anong satin ha",
+          "aba aba tinatag nga bat ba ha???!!!",
+          "sinasabi ko sayo iba ako magalit bat moba tinatag admin ko, ay be gusto mo ba owner ko?",
+          "hala sorry idolo wala pa si sam ih",
+          " hala hinahanap si sam miss mo ayieee.",
+        ];
+        return api.sendMessage(
+          { body: msg[Math.floor(Math.random() * msg.length)] },
+          event.threadID,
+          event.messageID,
+        );
+      }
     }
-    }}
+  }
 };
-module.exports.run = async function({}) {
-}
+module.exports.run = async function ({}) {};
